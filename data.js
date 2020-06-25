f5753bfea92517ad20abe5bf797b19f246e8312d
Mateusz Kalaga <46076250+mateuszkdev@users.noreply.github.com>
2020-06-25 15:03:23 +0200

    Add files via upload
    :O

---
+++ data.js
@@ -0,0 +1,67 @@
+const data = {
+
+    divs: {
+
+        'logo': 'SUPER STRONA W JS',
+
+        'timer': 'NULL : NULL : NULL | NULL . NULL . NULL',
+
+        'main': 
+        `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut magnam veritatis maxime minus neque dolores, repellat, consequuntur rem obcaecati deleniti accusantium dicta, explicabo eum laboriosam. Nihil, autem provident. Earum, rerum. <br>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut magnam veritatis maxime minus neque dolores, repellat, consequuntur rem obcaecati deleniti accusantium dicta, explicabo eum laboriosam. Nihil, autem provident. Earum, rerum. <br> 
+        `,
+
+
+
+    },
+
+    style: {
+
+        'logo':{
+
+            'color': 'black',
+            'background-color': 'white',
+            'font-size': '40px',
+            'text-align': 'center'
+
+        },
+
+        'main': {
+
+            'color': 'grey',
+            'background-color': 'white',
+            'text-align': 'center',
+            'margin-top': '20px',
+            'padding': '15px',
+
+        },
+
+        'body': {
+
+            'background-color': 'grey'
+
+        },
+
+        'timer': {
+
+            'text-align': 'left',
+            'background-color': 'white',
+            'margin': '10px',
+            'padding': '10px',
+            'font-size': '20px',
+            'color': 'grey',
+            'margin-right': '80%'
+
+        }
+
+    },
+
+    plugins: {
+
+        'timer': './plugins/timer.js'
+
+    }
+
+}
