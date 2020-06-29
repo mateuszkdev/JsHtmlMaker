ad1a25f9468a3635976fe9255b517a62c83a5e47
Mateusz Kalaga <46076250+mateuszkdev@users.noreply.github.com>
2020-06-29 11:14:22 +0200

    Nowa struktura divów ( podstrony )

--- data.js
+++ data.js
@@ -1,25 +1,59 @@
+const informacje_o_stronie = {
+
+    logo: 'SUPER STRONA W JS',
+    timer: '00:00:00'
+
+}
+
 const data = {
 
-    divs: {
+    title: 'Strona w JS',
 
-        'logo': 'SUPER STRONA W JS',
+    main_page: 'home',
 
-        'timer': 'NULL : NULL : NULL | NULL . NULL . NULL',
+    pages: {
 
-        'main': 
-        `
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut magnam veritatis maxime minus neque dolores, repellat, consequuntur rem obcaecati deleniti accusantium dicta, explicabo eum laboriosam. Nihil, autem provident. Earum, rerum. <br>
+        'home': {
 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut magnam veritatis maxime minus neque dolores, repellat, consequuntur rem obcaecati deleniti accusantium dicta, explicabo eum laboriosam. Nihil, autem provident. Earum, rerum. <br> 
-        `,
+            'logo': informacje_o_stronie.logo,
 
+            'timer': informacje_o_stronie.timer,
+    
+            'main': 
+            `
+            <p class="przycisk" onclick="zmien_strone('kontakt')"> KONTAKT - Kliknij </p> <br>
 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut magnam veritatis maxime minus neque dolores, repellat, consequuntur rem obcaecati deleniti accusantium dicta, explicabo eum laboriosam. Nihil, autem provident. Earum, rerum. <br>
+    
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut magnam veritatis maxime minus neque dolores, repellat, consequuntur rem obcaecati deleniti accusantium dicta, explicabo eum laboriosam. Nihil, autem provident. Earum, rerum. <br> 
+            `,
+
+        },
+
+        'kontakt': {
+
+            'logo': informacje_o_stronie.logo,
+
+            'timer': informacje_o_stronie.timer,
+
+            'main':
+            `
+            Kontakt: <br>
+            * E-Mail: XYZ@gmail.com <br>
+            <br>
+            *Telefon: 123 123 123
+
+            <p class="przycisk" onclick="zmien_strone('home')"> Wróć </p>
+            `
+
+        }
 
     },
 
+
     style: {
 
-        'logo':{
+        '#logo':{
 
             'color': 'black',
             'background-color': 'white',
@@ -28,7 +62,7 @@ const data = {
 
         },
 
-        'main': {
+        '#main': {
 
             'color': 'grey',
             'background-color': 'white',
@@ -44,7 +78,7 @@ const data = {
 
         },
 
-        'timer': {
+        '#timer': {
 
             'text-align': 'left',
             'background-color': 'white',
@@ -52,7 +86,21 @@ const data = {
             'padding': '10px',
             'font-size': '20px',
             'color': 'grey',
-            'margin-right': '80%'
+            'margin-right': '50%'
+
+        },
+
+        '.przycisk': {
+
+            'color': 'blue',
+            'margin': '0',
+            'font-size': '25px'
+
+        },
+
+        '.przycisk:hover': {
+
+            'color': 'red' 
 
         }
 
@@ -60,7 +108,9 @@ const data = {
 
     plugins: {
 
-        'timer': './plugins/timer.js'
+        'timer': './plugins/timer.js',
+        'divs': './plugins/divs.js',
+        'title': './plugins/title.js'
 
     }
 
