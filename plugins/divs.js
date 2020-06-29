f6aacaf23a3ce6b136d68ba46e2034541a7efe7a
Mateusz Kalaga <46076250+mateuszkdev@users.noreply.github.com>
2020-06-29 11:19:06 +0200

    hm

---
+++ plugins/divs.js
@@ -0,0 +1,12 @@
+const text = 
+`
+    Jestem divem dodanym w procesie po renderowania strony :)
+`
+
+const new_div = new div('main', 'nowy_div', text)
+
+new_div.style({
+
+    'color': 'yellow'
+
+})
